0dd608ae54b09a1000ef1cbe6cdc190ecf4da520
vaibhavbudhia <vaibhavbudhia7306@gmail.com>
2025-09-13 00:10:58 +0530

    header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -35,8 +35,8 @@ export function Header() {
             href="/"
             className="flex items-center space-x-2 transition-opacity hover:opacity-80"
           >
-            <AudioWaveform className="h-7 w-7 text-primary animate-pulse-gentle" />
-            <div className="flex flex-col">
+<img src = "https://github.com/vaibhavbudhia/MindEase-FrontEnd/blob/main/WhatsApp_Image_2025-09-07_at_22.49.51_0dbdf589-removebg-preview.png"></img>
+          <div className="flex flex-col">
               <span className="font-semibold text-lg bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-transparent">
                 Aura3.0
               </span>
